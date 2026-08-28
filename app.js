9429bd4247a982c34264acfaeeb66795a23d4998
jtsgai <jt.tuo@icloud.com>
2026-08-28 18:27:20 +0800

    Fix 1080p loader module import paths

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const origin = location.origin + location.pathname.replace(/[^/]+$/, "");
 const APP_SRC = "https://cdn.jsdelivr.net/gh/jtsgai/medhug-cabin@424f015159b7b2cca6cacb471eaf6aae439bc1ab/app.js";
 const code = (await fetch(APP_SRC).then((r) => {
   if (!r.ok) throw new Error("cannot load app core");
   return r.text();
 }))
   .replace('mirror:"auto",onRemoteStream', 'mirror:"auto",resolution:"1080p",onRemoteStream')
-  .replaceAll('from"./config.js"', 'from"./config.js?v=1080"')
-  .replaceAll('from"./i18n.js"', 'from"./i18n.js?v=1080"');
+  .replaceAll('from"./config.js"', `from"${origin}config.js"`)
+  .replaceAll('from"./i18n.js"', `from"${origin}i18n.js"`);
 await import(URL.createObjectURL(new Blob([code], { type: "text/javascript" })));
