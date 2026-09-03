5d801a32ba2924961babb1b3bab8f277b01d0a63
jtsgai <jt.tuo@icloud.com>
2026-09-03 14:03:50 +0800

    Cap TALK sessions at 30 seconds

--- talk/config.js
+++ talk/config.js
@@ -2,5 +2,5 @@
 export const LIVEAVATAR_EMBED_URL =
   "https://embed.liveavatar.com/v1/07d9bcd8-e677-4b26-8fc9-fbafb130a655?orientation=vertical";
 export const LIVEAVATAR_TOKEN_URL = "";
-// Free LiveAvatar plan sessions cap at 2 minutes.
-export const SESSION_MS = 2 * 60 * 1000;
+// Hard cap for this demo: 30 seconds, then the page tears down the iframe.
+export const SESSION_MS = 30 * 1000;
