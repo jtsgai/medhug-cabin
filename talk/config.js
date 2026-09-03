7b9acb13f962c00b906fca3335f35255ed1b9730
jtsgai <jt.tuo@icloud.com>
2026-09-03 15:59:38 +0800

    Switch TALK embed to Graham Black Suit portrait

--- talk/config.js
+++ talk/config.js
@@ -1,6 +1,6 @@
 // PUBLIC defaults for the cabin TALK page.
 export const LIVEAVATAR_EMBED_URL =
-  "https://embed.liveavatar.com/v1/07d9bcd8-e677-4b26-8fc9-fbafb130a655?orientation=vertical";
+  "https://embed.liveavatar.com/v1/3efcc958-aac8-4af2-802f-d4f4638c7210?orientation=vertical";
 export const LIVEAVATAR_TOKEN_URL = "";
 // Hard cap for this demo: 30 seconds, then the page tears down the iframe.
 export const SESSION_MS = 30 * 1000;
