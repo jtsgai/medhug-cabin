78e177bf6abe2b65988ba0d2d488ddfa9347622d
jtsgai <jt.tuo@icloud.com>
2026-09-03 16:39:43 +0800

    Cap TALK sessions at 1 minute

--- talk/config.js
+++ talk/config.js
@@ -2,5 +2,4 @@
 export const LIVEAVATAR_EMBED_URL =
   "https://embed.liveavatar.com/v1/3efcc958-aac8-4af2-802f-d4f4638c7210?orientation=vertical";
 export const LIVEAVATAR_TOKEN_URL = "";
-// Cabin cap. Free LiveAvatar plan also maxes a session at 2 minutes.
-export const SESSION_MS = 2 * 60 * 1000;
+export const SESSION_MS = 60 * 1000;
